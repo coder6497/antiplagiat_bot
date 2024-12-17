93f3195c4e2b0ba571d3e230c38f3890e4f6edcf
coder6497 <rty.sem@yandex.ru>
2024-12-17 18:19:45 +0300

    replace code to async

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
-import telebot
+from telebot.async_telebot import AsyncTeleBot
+from telebot import types
 from config import token
 from bot_algorhitms import write_file
+import asyncio
 
-bot = telebot.TeleBot(token)
-
+bot = AsyncTeleBot(token)
 
 @bot.message_handler(commands=["start"])
-def handle_start(message):
-    bot.send_message(message.chat.id, "Ай, что за кондиции!!!!!!")
+async def handle_start(message):
+    keyboard = types.ReplyKeyboardMarkup(row_width=2)
+    buttons = ["Отправитe текст 💬", "Отправить файл 📄"]
+    list(map(lambda x: keyboard.add(x), buttons))
+    await bot.send_message(message.chat.id, "Ай, что за кондиции!!!!!!", reply_markup=keyboard)
 
 
 @bot.message_handler(func=lambda message: True)
-def handle_message(message):
-    write_file(message.text)
-    bot.reply_to(message, "Текст отправлен на обработку")
+async def handle_message(message):
+    await write_file(message.text)
+    await bot.reply_to(message, "Текст отправлен на обработку")
 
 
-bot.polling(non_stop=True)
+asyncio.run(bot.polling(non_stop=True))
