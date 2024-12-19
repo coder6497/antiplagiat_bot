40ea8105b5c8ba0eb27218effc9252b1e1ef102f
Семён Бабушкин <rty.sem@yandex.ru>
2024-12-19 22:43:32 +0300

    refactor buttons

--- main.py
+++ main.py
@@ -24,21 +24,21 @@ async def handle_start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     show_text = types.KeyboardButton(buttons[0])
     for_gleb = types.KeyboardButton(buttons[3])
+    check_antiplag = types.KeyboardButton(buttons[1])
     markup.add(show_text)
     markup.add(for_gleb)
+    markup.add(check_antiplag)
     await bot.send_message(message.chat.id, "Введите тексты для проверки или посмотрите их", reply_markup=markup)
 
 
-@bot.message_handler(func=lambda message: True, content_types=['text', "photo"])
+@bot.message_handler(func=lambda message: True, content_types=['text'])
 async def handle_message(message):
     rows = conn.execute(db.select(texts).where(texts.columns.user_id == message.from_user.id)).fetchall()
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    check_antiplag = types.KeyboardButton(buttons[1])
     back = types.KeyboardButton(buttons[2])
-    markup.add(check_antiplag)
     markup.add(back)
     list(map(lambda x: markup.add(types.KeyboardButton(f'Удалить текст № {str(x.text_id)}')), rows))
-    if message.text not in buttons and message.text[:13] != "Удалить текст":
+    if message.text not in buttons and message.text[:13] != "Удалить текст" and message.text[:9] != "Проверить" and not message.text.isdigit():
         insertion_query = texts.insert().values({"text": '\n'.join(message.text.split('.')), "user_id": message.from_user.id})
         conn.execute(insertion_query)
         conn.commit()
@@ -51,7 +51,15 @@ async def handle_message(message):
         if message.text == f'Удалить текст № {str(row.text_id)}':
             conn.execute(texts.delete().where(texts.columns.text_id == row.text_id).where(texts.columns.user_id == message.from_user.id))
             conn.commit()
-            await bot.send_message(message.chat.id, f"Текст �� {row.text_id} удален", reply_markup=markup)
+            await bot.send_message(message.chat.id, f"Текст № {row.text_id} удален", reply_markup=markup)
+    if message.text == buttons[1]:
+        list(map(lambda x: markup.add(types.KeyboardButton(f'Проверить текст: {str(x.text_id)}')), rows))
+        await bot.reply_to(message, f"Выберите 2 номера которые надо сверить(пример 2 5)", reply_markup=markup)
+    if message.text[:16] == "Проверить текст:":
+            print(rows)
+
+
+
 
 
 asyncio.run(bot.infinity_polling())
