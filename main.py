a976a4932e10659bc777cb4bbb44124b3427168c
coder6497 <rty.sem@yandex.ru>
2024-12-20 00:27:07 +0300

    added so so working antiplagiat

--- main.py
+++ main.py
@@ -31,17 +31,18 @@ async def handle_start(message):
     await bot.send_message(message.chat.id, "Введите тексты для проверки или посмотрите их", reply_markup=markup)
 
 
-@bot.message_handler(func=lambda message: True, content_types=['text'])
+@bot.message_handler(func=lambda message: True, content_types=['text', 'photo'])
 async def handle_message(message):
     rows = conn.execute(db.select(texts).where(texts.columns.user_id == message.from_user.id)).fetchall()
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     back = types.KeyboardButton(buttons[2])
     markup.add(back)
     list(map(lambda x: markup.add(types.KeyboardButton(f'Удалить текст № {str(x.text_id)}')), rows))
-    if message.text not in buttons and message.text[:13] != "Удалить текст" and message.text[:9] != "Проверить" and not message.text.isdigit():
-        insertion_query = texts.insert().values({"text": '\n'.join(message.text.split('.')), "user_id": message.from_user.id})
-        conn.execute(insertion_query)
-        conn.commit()
+    if message.text not in buttons and message.text[:13] != "Удалить текст":
+        if len(rows) < 2:
+            insertion_query = texts.insert().values({"text": '\n'.join(message.text.split('.')), "user_id": message.from_user.id})
+            conn.execute(insertion_query)
+            conn.commit()
     if message.text == buttons[0]:
         for row in rows:
             await bot.send_message(message.chat.id, f"{row.text_id}.\t\n {row.text}", reply_markup=markup)
@@ -53,10 +54,21 @@ async def handle_message(message):
             conn.commit()
             await bot.send_message(message.chat.id, f"Текст № {row.text_id} удален", reply_markup=markup)
     if message.text == buttons[1]:
-        list(map(lambda x: markup.add(types.KeyboardButton(f'Проверить текст: {str(x.text_id)}')), rows))
-        await bot.reply_to(message, f"Выберите 2 номера которые надо сверить(пример 2 5)", reply_markup=markup)
-    if message.text[:16] == "Проверить текст:":
-            print(rows)
+            text1 = rows[0].text.split('\n')
+            text2 = rows[1].text.split('\n')
+            count = 0
+            for i in range(len(text1)):
+                for j in range(len(text2)):
+                    if text1[i] == text2[j]:
+                        count += 1
+            await bot.send_message(message.chat.id, f"Количество совпадений: {count}")
+            if count > 5:
+                await bot.send_message(message.chat.id, "Тексты совпадают на более чем 5 позиции, ПОДОЗРЕНИЕ НА ПЛАГИАТ")
+            if count > 10:
+                await bot.send_message(message.chat.id, "Тексты совпадают более чем на 10 позиции, ПЛАГИАТ")
+                await bot.send_photo(message.chat.id, open('для Глеба.png', 'rb'))
+            if count < 5:
+                await bot.send_message(message.chat.id, "Тексты не совпадают ни на одной позиции, НЕ ПЛАГИАТ")
 
 
 
